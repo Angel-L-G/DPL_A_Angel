f648f82dda9902c5acd311dbabef5485a29842ab
Angel-L-G <angelchiquitopons@gmail.com>
2024-09-25 14:58:15 +0100

    feat: T5 files;

---
+++ php/ej1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="get-post.php" method="get">
+        <label for="usuario">Nombre:</label>
+        <input type="text" name="usuario">
+        <br>
+        <input type="submit" name="enviar" value="enviar">
+    </form>
+</body>
+</html>
